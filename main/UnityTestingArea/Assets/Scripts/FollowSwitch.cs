e6177288b1f6b5615a301d3d7f0943496e89a974
sirerr <sirerr@gmail.com>
2014-07-13 20:13:51 -0400

    quick update to all

---
+++ main/UnityTestingArea/Assets/Scripts/FollowSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowSwitch : MonoBehaviour {
+
+	private GameObject[] flowerobj;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		flowerobj = GameObject.Find("Flower");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
