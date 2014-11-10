eb200c6bd96a4cd34229dee20cef2713b52dff74
sirerr <sirerr@gmail.com>
2014-11-09 20:30:32 -0500

    getting up to date

---
+++ main/UnityTestingArea/Assets/Ohiodev/bulletshoot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletshoot : MonoBehaviour {
+
+	public GameObject bullet;
+
+	public float bulletspeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+
+	 if(Input.GetKeyDown(KeyCode.F))
+		   {
+			GameObject bulletout;
+
+			bulletout = Instantiate(bullet,transform.position,transform.rotation) as GameObject;
+			bulletout.gameObject.rigidbody.AddForce(Vector3.forward *bulletspeed,ForceMode.Acceleration);
+		}
+	}
+}
